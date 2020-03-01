b598f2c25574504202ec2bfd82feabe7f9b8ae07
Vadym <volkov.vadym@gmail.com>
2020-03-01 17:09:27 +0200

    more change

--- main.py
+++ main.py
@@ -3,8 +3,64 @@ import os
 import shutil
 import filetype
 import piexif
+import exiftool
+from image_match.goldberg import ImageSignature
+gis = ImageSignature()
+
+a = gis.generate_signature('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg/687px-Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg')
+b = gis.generate_signature('https://pixabay.com/static/uploads/photo/2012/11/28/08/56/mona-lisa-67506_960_720.jpg')
+gis.normalized_distance(a, b)
 
 FINAL_DIR = 'D:/Vadym/Pictures/Sorted/'
+
+
+def get_exif(path):
+    files = [path]
+    with exiftool.ExifTool() as et:
+        metadata = et.get_metadata_batch(files)
+    data = metadata[0]
+    file_type = get_file_type(path)
+    date = 'other'
+    if file_type == 'image':
+        try:
+            date = format_date(data['EXIF:CreateDate'])
+            return date
+        except Exception as e:
+            print(e)
+        try:
+            date = format_date(data['File:FileModifyDate'])
+            return date
+        except Exception as e:
+            print(e)
+        try:
+            date = format_date(data['File:FileCreateDate'])
+            return date
+        except Exception as e:
+            print(e)
+    elif file_type == "video":
+        try:
+            date = format_date(data['QuickTime:CreateDate'])
+            return date
+        except Exception as e:
+            print(e)
+        try:
+            date = format_date(data['File:FileModifyDate'])
+            return date
+        except Exception as e:
+            print(e)
+    #for a in data:
+        #print(str(a) + ' - ' + str(data[a]))
+    return date
+
+
+def format_date(date):
+    date = date.split(' ')[0].strip()
+    date = date.split(":")
+    year = date[0]
+    month = date[1]
+    day = date[2]
+    date = year + "." + month + '.' + day
+    return date
 
 
 def get_date(path):
@@ -89,25 +145,42 @@ def copy_file(file, from_path, to_path):
         copy_file(new_name, from_path, to_path)
 
 
+def add_log(text):
+    log = open("log.txt", "a")
+    try:
+        log.write(text)
+        log.write('\n')
+    except Exception as e:
+        log.write("encode error: " + str(e))
+        log.write('\n')
+
+
 def scan(path):
     file_list = os.listdir(path)
     for file in file_list:
-        file_date = "other"
+        file_date = "unknown"
         new_path = path + file
         is_file = is_folder_or_file(new_path)
         if is_file:
             file_type = get_file_type(new_path)
             if file_type == 'image' or file_type == "video":
                 try:
-                    file_date = get_date_new(new_path)
+                    file_date = get_exif(new_path)
                 except Exception as e:
-                    print("ERROR")
-                    print(e)
+                    add_log("error")
+                    add_log(new_path)
                 create_dir(file_date)
                 copy_file(file, new_path, FINAL_DIR + file_date + "/")
+            else:
+                create_dir("another_format")
+                copy_file(file, new_path, FINAL_DIR + "another_format/")
         else:
             scan(new_path + '/')
 
-
-scan("D:/Vadym/Pictures/test3/")
-
+unknown = "D:/Vadym/Pictures/unknown/"
+test_folder = 'D:/Vadym/Pictures/test/'
+#scan("E:/Photo/")
+#scan(test_folder)
+#scan(unknown)
+#a = get_exif("test5.MPG")
+#print(a)
